7471e709129c8c54f7a977b1700f19e18dac7b2f
Panos68 <panosst68@gmail.com>
2020-08-21 09:27:57 +0200

    Changed docker compose and controller name

--- src/main/java/com/insiderlog/SupportCharts/controllers/SupportChartController.java
+++ src/main/java/com/insiderlog/SupportCharts/controllers/SupportChartController.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/supportChart")
+@RequestMapping(value = "/api/supportChart")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SupportChartController {
 
